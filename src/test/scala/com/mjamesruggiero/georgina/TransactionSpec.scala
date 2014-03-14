5ff843544fefede7c8cd85a198663274a84dfecb
mjamesruggiero <mjamesruggiero@gmail.com>
2014-03-13 18:08:11 -0700

    DRY up spec

--- src/test/scala/com/mjamesruggiero/georgina/TransactionSpec.scala
+++ src/test/scala/com/mjamesruggiero/georgina/TransactionSpec.scala
@@ -14,48 +14,38 @@ class TransactionSpec extends ScalatraSuite with FunSuite {
   }
 
   test("Transaction set can get average amount") {
-    val trans = List(
-      Transaction("2013-12-13", "debit", 1.00, "Amazon.com"),
-      Transaction("2013-12-13", "debit", 2.00, "Amazon.com"),
-      Transaction("2013-12-13", "debit", 3.00, "Amazon.com"),
-      Transaction("2013-12-13", "debit", 9.02, "Amazon.com")
+    val tSet = new TransactionSet(
+      List(
+        Transaction("2013-12-13", "debit", 1.00, "Amazon.com"),
+        Transaction("2013-12-13", "debit", 2.00, "Amazon.com"),
+        Transaction("2013-12-13", "debit", 3.00, "Amazon.com"),
+        Transaction("2013-12-13", "debit", 9.02, "Amazon.com")
+      )
     )
-    val tSet = new TransactionSet(trans)
     tSet.averageAmount should equal(3.755)
   }
 
-  test("Transaction set can get amount by species") {
-    val trans = List(
+  val largeTransSet = List(
       Transaction("2013-12-13", "debit", 100.00, "Amazon.com"),
       Transaction("2013-12-13", "debit", 200.00, "Amazon.com"),
-      Transaction("2013-12-13", "asset", 1000.00, "Big Company")
-    )
-    val tSet = new TransactionSet(trans)
+      Transaction("2013-12-13", "asset", 1000.00, "Big Company"),
+      Transaction("2013-12-13", "asset", 1000.00, "Big Company"),
+      Transaction("2013-12-13", "asset", 2000.00, "Big Company")
+  )
+
+  test("Transaction set can get amount by species") {
+    val tSet = new TransactionSet(largeTransSet)
     tSet.amountBySpecies("debit") should equal(300.00)
-    tSet.amountBySpecies("asset") should equal(1000.00)
+    tSet.amountBySpecies("asset") should equal(4000.00)
   }
 
   test("Transaction set can get average spend") {
-    val trans = List(
-      Transaction("2013-12-13", "debit", 100.00, "Amazon.com"),
-      Transaction("2013-12-13", "debit", 200.00, "Amazon.com"),
-      Transaction("2013-12-13", "debit", 300.00, "Amazon.com"),
-      Transaction("2013-12-13", "asset", 1000.00, "Big Company"),
-      Transaction("2013-12-13", "asset", 2000.00, "Big Company")
-    )
-    val tSet = new TransactionSet(trans)
-    tSet.averageSpend should equal(200.00)
+    val tSet = new TransactionSet(largeTransSet)
+    tSet.averageSpend should equal(150.00)
   }
 
   test("Transaction set can get standard deviation") {
-    val trans = List(
-      Transaction("2013-12-13", "debit", 100.00, "Amazon.com"),
-      Transaction("2013-12-13", "debit", 200.00, "Amazon.com"),
-      Transaction("2013-12-13", "debit", 300.00, "Amazon.com"),
-      Transaction("2013-12-13", "asset", 1000.00, "Big Company"),
-      Transaction("2013-12-13", "asset", 2000.00, "Big Company")
-    )
-    val tSet = new TransactionSet(trans)
-    tSet.standardDeviation("debit") should equal(81.64965809277261)
+    val tSet = new TransactionSet(largeTransSet)
+    tSet.standardDeviation("debit") should equal(50.0)
   }
 }
