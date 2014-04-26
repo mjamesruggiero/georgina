95a43f425cd5343ec65209b16abc475290e7c00a
mjamesruggiero <mjamesruggiero@gmail.com>
2014-04-26 09:36:08 -0700

    add categorizers
    * TODO use trait fro Strategy
    * TODO extract the map to JSON

--- src/test/scala/com/mjamesruggiero/georgina/CategorizerSpec.scala
+++ src/test/scala/com/mjamesruggiero/georgina/CategorizerSpec.scala
@@ -16,17 +16,75 @@ class CategorizerSpec extends ScalatraSuite with FunSuite {
   }
 
   val testMap = Map(
-    "check" -> "CHECK # 2045",
-    "grocery" -> "POS PURCHASE - NOB HILL 632 ALAMEDA CA 1523 00463171029909340",
-    "atm" -> "ATM WITHDRAWAL - 2314 SANTA CLARA AVE ALAMEDA CA 6604 0008437",
-    "amazon" -> "CHECK CRD PURCHASE 06/18 AMAZON MKTPLACE PM AMZN.COM/BILL WA 446024XXXXXX1523 083169177896272 ?MCC=5942",
-    "utilities" -> "BILL PAY ALAMEDA POWER AN ON-LINE xx834-01 ON 06-18"
+    "ALAMEDA ALLIANCE PAYROLL" -> "salary",
+    "ALAMEDA GAS" -> "transportation",
+    "ALAMEDA NATURAL" -> "grocery",
+    "ALAMEDA POWER" -> "utilities",
+    "ATM WITHDRAWAL - 2314 SANTA CLARA AVE ALAMEDA CA 6604 0008437" -> "atm",
+    "AVEDA" -> "household",
+    "BILL PAY ALAMEDA COUNTY I" -> "utilities",
+    "BILL PAY ALAMEDA POWER AN ON-LINE xx834-01 ON 06-18" -> "utilities",
+    "BILL PAY SBC" -> "utilities",
+    "BOOKS INC" -> "entertainment",
+    "BOUNCE" -> "clothing",
+    "CA DMV" -> "transportation",
+    "CHECK # 2045" -> "check",
+    "CHECK CRD PURCHASE 06/18 AMAZON MKTPLACE PM AMZN.COM/BILL WA 446024XXXXXX1523 083169177896272 ?MCC=5942" -> "amazon",
+    "CHECK DEPOSIT" -> "asset",
+    "COLE HARDWARE" -> "household",
+    "COSTPLUS" -> "household",
+    "CREDOMOBILE" -> "utilities",
+    "CVS" -> "household",
+    "DESTROY ALL SOFTWARE" -> "entertainment",
+    "EAST BAY MUNICIP" -> "utilities",
+    "EDWARD JONES" -> "savings",
+    "ENCINAL MARKET" -> "grocery",
+    "ENGINE WORKS" -> "transportation",
+    "FEEL GOOD BAKERY" -> "dining",
+    "GITHUB" -> "utilities",
+    "HSBC ONLINE TRANSFER" -> "savings",
+    "IKEA" -> "household",
+    "INTEREST PAYMENT" -> "interest",
+    "JAZZERCISE" -> "health and exercise",
+    "LAMORINDA SPANIS" -> "education",
+    "LOVE AT FIRST BITE" -> "dining",
+    "NETFLIX.COM" -> "entertainment",
+    "NOB HILL" -> "grocery",
+    "OFFICE MAX" -> "household",
+    "OLD NAVY" -> "clothing",
+    "PACIFIC GAS" -> "utilities",
+    "PAYPAL" -> "paypal",
+    "PEET'S" -> "grocery",
+    "POPPY RED" -> "entertainment",
+    "POS PURCHASE - NOB HILL 632 ALAMEDA CA 1523 00463171029909340" -> "grocery",
+    "PURCHASE - KOHL" -> "household",
+    "RAILSCASTS" -> "entertainment",
+    "REDBUBBLE" -> "clothes",
+    "REILLY MEDIA" -> "entertainment",
+    "RESTAURANT" -> "dining",
+    "SAFEWAY" -> "grocery",
+    "SHARETHROUGH INC DIRECT" -> "salary",
+    "SHELL OIL" -> "transportation",
+    "TARGET" -> "household",
+    "THE MELT-EMBARCADE" -> "dining",
+    "TJ MAXX" -> "clothing",
+    "TO SAVINGS" -> "savings",
+    "TOT TANK" -> "household",
+    "TOY SAFARI" -> "entertainment",
+    "TREASURY DIRECT" -> "savings",
+    "VALERO" -> "transportation",
+    "VISA WELLS" -> "credit card",
+    "WALGREEN" -> "household",
+    "WHOLE FOODS" -> "grocery",
+    "WILDFLOWER CAFE" -> "dining",
+    "WITHDRAWAL IN BRANCH" -> "cash",
+    "YOSHI" -> "entertainment"
   )
 
   test("Categorizer should match basic examples") {
     for ((k, v) <- testMap) {
-      val c = new Categorizer(v).categorize  
-      c should equal(Category(k))
+      val c = new Categorizer(k).categorize
+      c should equal(Category(v))
     }
   }
 }
