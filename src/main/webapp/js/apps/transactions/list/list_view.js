3e6a079ae3a697ccd38c2bc35fa38bb78b8d5fa7
mjamesruggiero <mjamesruggiero@gmail.com>
2014-05-13 21:03:30 -0700

    build table with CompositeView

--- src/main/webapp/js/apps/transactions/list/list_view.js
+++ src/main/webapp/js/apps/transactions/list/list_view.js
@@ -1,15 +1,29 @@
 var transactionListTemplate =
     _.template("<td><%- date.substring(0, 10) %></td><td><%- category %></td><td><%- description.substring(0, 48) %>...</td><td><%- amount %></td>");
 
+var transactionTableMarkup = "<thead>"+
+"<thead>" +
+    "<tr>" +
+        "<th>Date</th>" +
+        "<th>Category</th>" +
+        "<th>Description</th>" +
+        "<th>Amount</th>" +
+    "</tr>" +
+"</thead>" +
+"<tbody>" + "</tbody>" + "</thead>";
+var transactionTableTemplate = _.template(transactionTableMarkup);
+
 Georgina.module("TransactionsApp.List", function(List, Georgina, Backbone, Marionette, $, _) {
     List.Transaction = Marionette.ItemView.extend({
         tagName: "tr",
         template: transactionListTemplate
     });
 
-    List.Transactions = Marionette.CollectionView.extend({
+    List.Transactions = Marionette.CompositeView.extend({
         tagName: "table",
         className: "table table-condensed",
-        itemView: List.Transaction
+        template: transactionTableTemplate,
+        itemView: List.Transaction,
+        itemViewContainer: "tbody"
     });
 });
