1c27932ceb412d2914300b3a941e31beebd5a584
mjamesruggiero <mjamesruggiero@gmail.com>
2014-05-15 21:26:03 -0700

    migrate last of the JS templates

--- src/main/webapp/js/apps/transactions/list/list_view.js
+++ src/main/webapp/js/apps/transactions/list/list_view.js
@@ -1,28 +1,7 @@
-var transactionItemMarkup = "<td><%- date.substring(0, 10) %></td>"+
-"<td><%- category %></td>"+
-"<td><%- description.substring(0, 40) %>...</td>"+
-"<td><%- amount %></td>"+
-"<td><a href='#' class='btn btn-info btn-sm js-show'><i class='icon-zoom-in'></i>More</a></td>";
-
-var transactionItemTemplate = _.template(transactionItemMarkup);
-
-var transactionTableMarkup = "<thead>"+
-"<thead>" +
-    "<tr>" +
-        "<th>Date</th>" +
-        "<th>Category</th>" +
-        "<th>Description</th>" +
-        "<th>Amount</th>" +
-        "<th></th>" +
-    "</tr>" +
-"</thead>" +
-"<tbody>" + "</tbody>" + "</thead>";
-var transactionTableTemplate = _.template(transactionTableMarkup);
-
 Georgina.module("TransactionsApp.List", function(List, Georgina, Backbone, Marionette, $, _) {
     List.Transaction = Marionette.ItemView.extend({
         tagName: "tr",
-        template: transactionItemTemplate,
+        template: "#transaction-item",
         events: {
             "click": "highlightRow",
             "click td a.js-show": "showClicked"
@@ -40,7 +19,7 @@ Georgina.module("TransactionsApp.List", function(List, Georgina, Backbone, Mario
     List.Transactions = Marionette.CompositeView.extend({
         tagName: "table",
         className: "table table-condensed",
-        template: transactionTableTemplate,
+        template: "#transaction-table",
         itemView: List.Transaction,
         itemViewContainer: "tbody"
     });
--- src/main/webapp/js/index.js
+++ src/main/webapp/js/index.js
@@ -1,24 +1,3 @@
-// Templates -
-// rendering here and not on the
-// Scala Server Pages - the "<>%%>" tags
-// clash with scalatra templating
-var summaryTemplateMarkup = "<tr> <td>Start Date</td>" +
-        "<td><%- startDate %></td> "+
-        "<td>End Date</td>"+
-        "<td><%- endDate %></td>"+
-        "<td>Category</td><td><%- category %></td>"+
-        "<td>Mean</td><td><%- mean %></td>"+
-        "<td>Count</td><td><%- count %></td>+"+
-        "<td>Standard deviation</td>"+
-        "<td><%- standard_deviation %></td></tr>";
-var summaryTemplate = _.template(summaryTemplateMarkup);
-var summaryModalMarkup = "<button class='btn btn-primary' data-toggle='modal' data-target='.bs-example-modal-lg'>Large modal</button>"+
-        " <div class='modal fade bs-example-modal-lg' tabindex='-1' role='dialog' aria-labelledby='myLargeModalLabel' aria-hidden='true'> "+
-        "<div class='modal-dialog modal-lg'> "+
-        "<div class='modal-content'> ...  </div> </div> </div>";
-var summaryModalTemplate = _.template(summaryModalMarkup);
-
-///// categories
 Georgina.SummaryItemView = Marionette.ItemView.extend({
     tagName: "tr",
     template: summaryTemplate
