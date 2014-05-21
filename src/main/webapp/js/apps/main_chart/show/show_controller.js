67285ccc63c5cdaa43bb4c41059d1fabe9fbb662
mjamesruggiero <mjamesruggiero@gmail.com>
2014-05-20 18:11:32 -0700

    remove unnecessary console.log

--- src/main/webapp/js/apps/main_chart/show/show_controller.js
+++ src/main/webapp/js/apps/main_chart/show/show_controller.js
@@ -2,7 +2,6 @@ Georgina.module("MainChartApp.Show", function(Show, Georgina, Backbone, Marionet
     Show.Controller = {
         showMainChart: function() {
             var chartData = Georgina.request("main_chart:entities");
-            console.log("data is ", chartData);
             var mainChartView = new Show.MainChart({
                 model: chartData
             });
