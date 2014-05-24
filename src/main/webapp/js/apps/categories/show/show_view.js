1957799d90b7a2034b933b364fa613e7284e4037
mjamesruggiero <mjamesruggiero@gmail.com>
2014-05-23 23:52:20 -0700

    summary panel has a "back" button

--- src/main/webapp/js/apps/categories/show/show_view.js
+++ src/main/webapp/js/apps/categories/show/show_view.js
@@ -1,6 +1,15 @@
 Georgina.module("CategoriesApp.Show", function(Show, Georgina, Backbone, Marionette, $, _){
     Show.Category = Marionette.ItemView.extend({
-        template: "#summary-view"
+        template: "#summary-view",
+        events: {
+            "click a.js-back": "showTransactions"
+        },
+        showTransactions: function(e){
+          console.log("you triggered a back");
+          e.preventDefault();
+          e.stopPropagation();
+          Georgina.trigger("transactions:list");
+        }
     });
 });
 
