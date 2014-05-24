c428caa6ba95537dc6e8b0142ba3ebf93bb9d286
mjamesruggiero <mjamesruggiero@gmail.com>
2014-05-24 09:12:19 -0700

    transaction view has back button

--- src/main/webapp/js/apps/transactions/show/show_view.js
+++ src/main/webapp/js/apps/transactions/show/show_view.js
@@ -1,5 +1,14 @@
 Georgina.module("TransactionsApp.Show", function(Show, Georgina, Backbone, Marionette, $, _){
     Show.Transaction = Marionette.ItemView.extend({
-        template: "#transaction-view"
+        template: "#transaction-view",
+
+        events: {
+            "click a.js-transaction-back": "showTransactions"
+        },
+        showTransactions: function(e) {
+            e.preventDefault();
+            e.stopPropagation();
+            Georgina.trigger("transactions:list");
+        }
     });
 });
