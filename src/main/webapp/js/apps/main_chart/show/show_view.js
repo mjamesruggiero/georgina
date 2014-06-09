307775a0863e041f9b1529d42cbb19ac95a985fc
mjamesruggiero <mjamesruggiero@gmail.com>
2014-06-09 12:46:53 -0700

    Backbone Marionette is removed

--- src/main/webapp/js/apps/main_chart/show/show_view.js
+++
@@ -1,7 +0,0 @@
-Georgina.module("MainChartApp.Show", function(Show, Georgina, Backbone, Marionette, $, _){
-    Show.MainChart = Marionette.ItemView.extend({
-        template: "#main-chart-view"
-    });
-});
-
-
