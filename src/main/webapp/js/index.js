485cbfb8a9d98a922e7e2f113693ddb9fd728e1c
mjamesruggiero <mjamesruggiero@gmail.com>
2014-06-11 21:04:30 -0700

    refactor for isolation

--- src/main/webapp/js/index.js
+++ src/main/webapp/js/index.js
@@ -1,1 +1,15 @@
-Georgina.start();
+// the table
+var rows = Components.templatedStrings(
+    $("#transaction-item").html(),
+    Components.getData("transactions/")
+);
+$("#main-table tbody").append(rows);
+
+// the chart
+var options = {
+    w: 740, h: 250,
+    barColor: "green",
+    chartRegion: "#main-chart-region"
+}
+var munged = _.map(Components.getData("/reports/byday"), function(r) { return r.total; });
+Components.lineChart(munged, options);
